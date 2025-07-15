0bce7e2c933c934998e6cbd68f4017a6df116fc7
Carlos Castillo <cacr1990@gmail.com>
2025-07-14 23:55:15 -0400

    feat: add protected route for tasks with authentication guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 // src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { authGuard } from './auth/auth-guard';
 
 export const routes: Routes = [
   // Rutas de autenticación (públicas)
@@ -14,13 +15,13 @@ export const routes: Routes = [
       import('./auth/auth-module').then((m) => m.AuthModule),
   },
 
-  // Aquí luego añadirás las rutas protegidas (Tasks, Users, etc.)
-  // {
-  //   path: 'tasks',
-  //   canActivate: [AuthGuard],
-  //   loadChildren: () =>
-  //     import('./tasks/tasks-module').then((m) => m.TasksModule),
-  // },
+  // Rutas protegidas de tareas
+  {
+    path: 'tasks',
+    canActivate: [authGuard],
+    loadChildren: () =>
+      import('./tasks/tasks-module').then((m) => m.TasksModule),
+  },
 
   // Redirección por defecto
   { path: '', redirectTo: 'login', pathMatch: 'full' },
